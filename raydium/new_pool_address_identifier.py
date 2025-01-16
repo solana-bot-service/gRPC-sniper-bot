077177e577481649c7e9391efb58dc1c4a74374e
cashblaze127 <cashblaze129@gmail.com>
2025-01-16 11:05:16 +0700

    Update new_pool_address_identifier.py

--- raydium/new_pool_address_identifier.py
+++ raydium/new_pool_address_identifier.py
@@ -7,7 +7,7 @@ def get_pair_address_new_pool(ctx, sig):
         sig1 = Signature.from_string(sig)
         instructions = ctx.get_transaction(sig1,encoding="jsonParsed", max_supported_transaction_version=0).value.transaction.transaction.message.instructions
         for ins in instructions:
-            if "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8" in str(ins.program_id):
+            if "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8" in str(ins.program_id): 
                 pair_address = str(ins.accounts[4])
         return pair_address 
     except:
